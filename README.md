19f92f424c05a221d514fb0043869a6307c1f2de
Diane Doan <dianeddoan@gmail.com>
2026-09-21 12:46:40 -0600

    Add hash and set methods

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # HashMap
-
+This project contains the `HashMap` class containing the following functions:
+- `hash(key)` takes a string key and produces a hash code with it
+- `set(key, value)` takes a key and a value to associate with the key
+  - If the key already exists in the hash map, then the old value associated with it is overwritten with the new one
 
 ## Description
 This project demonstrates the skills learnt from the JavaScript course in The Odin Project up until the [HashMap Data Structure](https://www.theodinproject.com/lessons/javascript-hashmap-data-structure) topic. 
--- hashMap.js
+++ hashMap.js
@@ -1,3 +1,42 @@
 class HashMap {
+  constructor(loadFactor = 0.75, capacity = 16) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.buckets = [];
+    for (let i = 0; i < capacity; i++) {
+      this.buckets.push([]);
+    }
+  }
+
+  // take a string key and produces a hash code with it
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity; // ensure index fits in the buckets
+    }
+
+    return hashCode;
+  }
+
+  // take a key and a value to associate with the key
+  set(key, value) {
+    const index = this.hash(key); // use hash code as index of bucket array to store the key-value pair
+    const bucket = this.buckets[index]; // find bucket at index
+
+    // if key already exists in hash map,  overwrite old value with new one
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket[i][1] = value;
+        return;
+      }
+    }
+
+    // otherwise store key-value pair in bucket
+    bucket.push([key, value]);
+  }
   
 }
+
+export { HashMap };
